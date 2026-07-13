5873bbdde0f242c9edfd83b38648b5911c44c603
Felix Freitas Jr <118613377+FelixFreitasJr@users.noreply.github.com>
2026-07-13 03:30:32 +0000

    feat: implementa classe Veiculo e instancia no Main. Closes #6

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,5 +1,7 @@
 package app;
 
+import model.Veiculo;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,6 +9,16 @@ public class Main {
         System.out.println("==================================");
         System.out.println("        AutoZelo v0.1");
         System.out.println("==================================");
+ 
+          Veiculo meuCarro = new Veiculo(
+                "Chevrolet",
+                "Corsa Millennium",
+                2001,
+                "KNS-1977",
+                144000
+        );
+
+        System.out.println(meuCarro);
 
     }
 
--- src/model/Veiculo.java
+++ src/model/Veiculo.java
@@ -2,4 +2,21 @@ package model;
 
 public class Veiculo {
 
+    private String marca;
+    private String modelo;
+    private int anoFabricacao;
+    private String placa;
+    private int quilometragem;
+
+
+
+public Veiculo(String marca, String modelo, int anoFabricacao, String placa, int quilometragem) {
+
+    this.marca = marca;
+    this.modelo = modelo;
+    this.anoFabricacao = anoFabricacao;
+    this.placa = placa;
+    this.quilometragem = quilometragem;
+
+}
 }
