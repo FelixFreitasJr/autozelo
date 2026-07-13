b4399b7be0fbe058ab88a524970e575832b7a43d
Felix Freitas Jr <118613377+FelixFreitasJr@users.noreply.github.com>
2026-07-13 03:11:39 +0000

    feat: cria estrutura inicial do projeto Java. Closes #4

---
+++ src/app/Main.java
@@ -0,0 +1,13 @@
+package app;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("==================================");
+        System.out.println("        AutoZelo v0.1");
+        System.out.println("==================================");
+
+    }
+
+}
